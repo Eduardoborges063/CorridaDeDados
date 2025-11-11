969722e16844948a216d30b7349df7d17ee89953
Eduardoborges063 <1eduardomacedo2525@gmail.com>
2025-11-11 19:17:42 -0300

    Adicionar arquivos de projeto.

---
+++ CorridaDeDados.ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CorridaDeDados.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
